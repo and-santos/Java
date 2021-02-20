8923f179d8d0096662423e804a4e1f695abbc15c
André Santos <72099036+and-santos@users.noreply.github.com>
2021-02-19 21:06:42 -0300

    Add files via upload

---
+++ media.java
@@ -0,0 +1,12 @@
+public class media {
+    public static void main(String[] args) {
+        double N1, N2, N3, N4, RES;
+        N1 = Double.parseDouble(System.console().readLine());
+        N2 = Double.parseDouble(System.console().readLine());
+        N3 = Double.parseDouble(System.console().readLine());
+        N4 = Double.parseDouble(System.console().readLine());
+
+        RES = (N1+N2+N3+N4) / 4;
+        System.out.println(RES);
+    }
+}
